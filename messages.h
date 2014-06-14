ed09f374b13edaedf99ad6cd6512f750acde5981
Skillinator <Skillinalex@gmail.com>
2014-06-13 20:45:12 -0500

    Create messages.h, add DeltaMessage

---
+++ messages.h
@@ -0,0 +1,19 @@
+#ifndef _skilLibMessaging
+#define _skilLibMessaging
+
+#include <stdio.h>
+#include <vector>
+#include <GLFW/glfw3.h>
+#include <string>
+#include "skilLib.h"
+#include "systems.h"
+
+const int MESSAGE_DELTA = 1;
+
+class DeltaMessage : public Message{
+  DeltaMessage();
+  DeltaMessage(float d);
+  float delta;
+}
+
+#endif
